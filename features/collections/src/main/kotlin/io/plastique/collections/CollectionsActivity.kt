190cccaf1984b9d25405ab1abd45a6367ae3fc04
technoir <sergey.chelombitko@gmail.com>
2018-10-12 00:37:13 +0300

    Scroll collections to top when Toolbar is clicked

--- features/collections/src/main/kotlin/io/plastique/collections/CollectionsActivity.kt
+++ features/collections/src/main/kotlin/io/plastique/collections/CollectionsActivity.kt
@@ -3,26 +3,42 @@ package io.plastique.collections
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import io.plastique.core.BaseActivity
 import io.plastique.core.extensions.setActionBar
+import io.plastique.core.extensions.setSubtitleOnClickListener
+import io.plastique.core.extensions.setTitleOnClickListener
 import io.plastique.inject.getComponent
 
 class CollectionsActivity : BaseActivity() {
+    private lateinit var contentFragment: CollectionsFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collections)
 
-        val username = intent.getStringExtra(EXTRA_USERNAME)!!
-        setActionBar(R.id.toolbar) {
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        initToolbar(username)
+
+        if (savedInstanceState == null) {
+            contentFragment = CollectionsFragment.newInstance(username)
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.collections_container, contentFragment)
+                    .commit()
+        } else {
+            contentFragment = supportFragmentManager.findFragmentById(R.id.collections_container) as CollectionsFragment
+        }
+    }
+
+    private fun initToolbar(username: String?) {
+        val toolbar = setActionBar(R.id.toolbar) {
             subtitle = username
             setDisplayHomeAsUpEnabled(true)
         }
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .add(R.id.collections_container, CollectionsFragment.newInstance(username))
-                    .commit()
-        }
+        val listener = View.OnClickListener { contentFragment.scrollToTop() }
+        toolbar.setTitleOnClickListener(listener)
+        toolbar.setSubtitleOnClickListener(listener)
     }
 
     override fun injectDependencies() {
