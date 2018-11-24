0a4ac620634a632f2cf104cc219962951b978102
technoir <sergey.chelombitko@gmail.com>
2018-11-25 01:18:22 +0300

    Fix bug where margins were incorrect after item move

--- features/collections/src/main/kotlin/io/plastique/collections/FolderItem.kt
+++ features/collections/src/main/kotlin/io/plastique/collections/FolderItem.kt
@@ -3,10 +3,8 @@ package io.plastique.collections
 import io.plastique.core.lists.IndexedItem
 import io.plastique.core.lists.ListItem
 
-data class FolderItem(val folder: Folder) : ListItem, IndexedItem {
+data class FolderItem(val folder: Folder, override var index: Int = 0) : ListItem, IndexedItem {
     override val id: String get() = folder.id
-
-    override var index: Int = 0
 }
 
 data class HeaderItem(
--- features/deviations/src/main/kotlin/io/plastique/deviations/list/DeviationItem.kt
+++ features/deviations/src/main/kotlin/io/plastique/deviations/list/DeviationItem.kt
@@ -4,8 +4,6 @@ import io.plastique.core.lists.IndexedItem
 import io.plastique.core.lists.ListItem
 import io.plastique.deviations.Deviation
 
-data class DeviationItem(val deviation: Deviation) : ListItem, IndexedItem {
+data class DeviationItem(val deviation: Deviation, override var index: Int = 0) : ListItem, IndexedItem {
     override val id: String get() = deviation.id
-
-    override var index: Int = 0
 }
