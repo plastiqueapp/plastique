7285e595363b836af2c881d77cdc9b8b636a4058
technoir <sergey.chelombitko@gmail.com>
2018-11-28 17:51:20 +0300

    Change User.type to enum

--- core/src/main/kotlin/io/plastique/users/User.kt
+++ core/src/main/kotlin/io/plastique/users/User.kt
@@ -3,8 +3,28 @@ package io.plastique.users
 data class User(
     val id: String,
     val name: String,
-    val type: String,
+    val type: UserType,
     val avatarUrl: String
 )
 
-fun UserEntity.toUser(): User = User(id = id, name = name, type = type, avatarUrl = avatarUrl)
+enum class UserType {
+    Regular,
+    Group,
+    Core,
+    Banned
+}
+
+fun UserEntity.toUser(): User {
+    val userType = when (type) {
+        "premium",
+        "senior",
+        "beta",
+        "hell",
+        "hell-beta",
+        "admin" -> UserType.Core
+        "group" -> UserType.Group
+        "banned" -> UserType.Banned
+        else -> UserType.Regular
+    }
+    return User(id = id, name = name, type = userType, avatarUrl = avatarUrl)
+}
