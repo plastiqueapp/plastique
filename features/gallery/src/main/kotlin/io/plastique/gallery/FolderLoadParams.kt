78b37c8e32cfa3c30cf91c3b16e6b7e90f3ce5ef
technoir <sergey.chelombitko@gmail.com>
2019-01-16 00:57:01 +0300

    Generate adapter for FolderLoadParams

--- features/gallery/src/main/kotlin/io/plastique/gallery/FolderLoadParams.kt
+++ features/gallery/src/main/kotlin/io/plastique/gallery/FolderLoadParams.kt
@@ -1,6 +1,13 @@
 package io.plastique.gallery
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class FolderLoadParams(
+    @Json(name = "username")
     val username: String?,
+
+    @Json(name = "show_mature")
     val matureContent: Boolean
 )
