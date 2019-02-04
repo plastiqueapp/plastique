eb16b9b5666eeced9a8e111a26c854e852d2e7dc
technoir <sergey.chelombitko@gmail.com>
2019-02-04 21:40:57 +0300

    Ensure that toolbar actually exists in setActionBar

--- core-ui/src/main/kotlin/io/plastique/core/extensions/ActivityExt.kt
+++ core-ui/src/main/kotlin/io/plastique/core/extensions/ActivityExt.kt
@@ -4,9 +4,10 @@ import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat.requireViewById
 
 fun AppCompatActivity.setActionBar(@IdRes id: Int, init: ActionBar.() -> Unit = {}): Toolbar {
-    val toolbar = findViewById<Toolbar>(id)
+    val toolbar = requireViewById<Toolbar>(this, id)
     setSupportActionBar(toolbar)
     supportActionBar!!.init()
     return toolbar
