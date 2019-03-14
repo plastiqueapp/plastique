8e0f4bbd4990b5afb1f55751c6203aa98ee55411
technoir <sergey.chelombitko@gmail.com>
2019-03-14 20:02:30 +0300

    Ensure that joined strings don't contain the delimiter

--- core/src/main/kotlin/io/plastique/core/converters/StringListConverter.kt
+++ core/src/main/kotlin/io/plastique/core/converters/StringListConverter.kt
@@ -5,11 +5,16 @@ import androidx.room.TypeConverter
 class StringListConverter {
     @TypeConverter
     fun toString(list: List<String>): String {
-        return list.joinToString(",")
+        list.forEach { if (it.contains(DELIMITER)) throw IllegalArgumentException("$it must not contain delimiter $DELIMITER") }
+        return list.joinToString(DELIMITER)
     }
 
     @TypeConverter
     fun fromString(value: String): List<String> {
-        return if (value.isNotEmpty()) value.split(",") else emptyList()
+        return if (value.isNotEmpty()) value.split(DELIMITER) else emptyList()
+    }
+
+    companion object {
+        private const val DELIMITER = "|"
     }
 }
