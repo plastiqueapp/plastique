32f7e773dcd25ff2e2a04e716fc5e940c3e2ac3f
technoir <sergey.chelombitko@gmail.com>
2019-05-28 23:54:18 +0300

    Enable Flipper database plugin

--- app/src/debug/kotlin/io/plastique/inject/modules/DebuggingModule.kt
+++ app/src/debug/kotlin/io/plastique/inject/modules/DebuggingModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.core.FlipperClient
 import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
@@ -23,6 +24,7 @@ object DebuggingModule {
         flipperClient.addPlugin(NetworkFlipperPlugin())
         flipperClient.addPlugin(SharedPreferencesFlipperPlugin(context))
         flipperClient.addPlugin(CrashReporterPlugin.getInstance())
+        flipperClient.addPlugin(DatabasesFlipperPlugin(context))
         return flipperClient
     }
 }
