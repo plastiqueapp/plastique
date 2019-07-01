cc2a2a626818f6265fc1ea2afe65b6a57bde1a7c
technoir <sergey.chelombitko@gmail.com>
2019-07-01 18:20:17 +0300

    Add ability to pass options to NavigationContext.startActivity

--- libraries/core-ui/src/main/kotlin/io/plastique/core/navigation/NavigationContext.kt
+++ libraries/core-ui/src/main/kotlin/io/plastique/core/navigation/NavigationContext.kt
@@ -3,20 +3,21 @@ package io.plastique.core.navigation
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 interface NavigationContext {
     val context: Context
 
-    fun startActivity(intent: Intent)
+    fun startActivity(intent: Intent, options: Bundle? = null)
 }
 
 private class ActivityNavigationContext(private val activity: Activity) : NavigationContext {
     override val context: Context
         get() = activity
 
-    override fun startActivity(intent: Intent) {
-        activity.startActivity(intent)
+    override fun startActivity(intent: Intent, options: Bundle?) {
+        activity.startActivity(intent, options)
     }
 }
 
@@ -24,8 +25,8 @@ private class FragmentNavigationContext(private val fragment: Fragment) : Naviga
     override val context: Context
         get() = fragment.requireContext()
 
-    override fun startActivity(intent: Intent) {
-        fragment.startActivity(intent)
+    override fun startActivity(intent: Intent, options: Bundle?) {
+        fragment.startActivity(intent, options)
     }
 }
 
