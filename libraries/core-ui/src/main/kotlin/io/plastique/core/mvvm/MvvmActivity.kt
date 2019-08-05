24eb60901f465add4e98e7be7510624923cec7e0
technoir <sergey.chelombitko@gmail.com>
2019-08-06 00:23:23 +0300

    Remove unused field

--- libraries/core-ui/src/main/kotlin/io/plastique/core/mvvm/MvvmActivity.kt
+++ libraries/core-ui/src/main/kotlin/io/plastique/core/mvvm/MvvmActivity.kt
@@ -1,14 +1,10 @@
 package io.plastique.core.mvvm
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import io.plastique.core.BaseActivity
-import javax.inject.Inject
 
 abstract class MvvmActivity<VM : BaseViewModel>(private val viewModelClass: Class<VM>) : BaseActivity() {
-    @Inject protected lateinit var viewModelFactory: ViewModelProvider.Factory
-
     protected val viewModel: VM by lazy(LazyThreadSafetyMode.NONE) {
         ViewModelProviders.of(this, activityComponent.viewModelFactory()).get(viewModelClass)
     }
