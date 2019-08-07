824196d35c3f10d1213c4803506f2d5c1504d799
technoir <sergey.chelombitko@gmail.com>
2019-08-08 00:45:34 +0300

    LeakCanary 2.0

--- app/src/debug/kotlin/io/plastique/inject/modules/DebugInitializerModule.kt
+++ app/src/debug/kotlin/io/plastique/inject/modules/DebugInitializerModule.kt
@@ -5,7 +5,6 @@ import dagger.Module
 import dagger.multibindings.IntoSet
 import io.plastique.core.init.FlipperInitializer
 import io.plastique.core.init.Initializer
-import io.plastique.core.init.LeakCanaryInitializer
 import io.plastique.core.init.StethoInitializer
 import io.plastique.core.init.TimberInitializer
 
@@ -14,10 +13,6 @@ interface DebugInitializerModule {
     @Binds
     @IntoSet
     fun bindFlipperInitializer(impl: FlipperInitializer): Initializer
-
-    @Binds
-    @IntoSet
-    fun bindLeakCanaryInitializer(impl: LeakCanaryInitializer): Initializer
 
     @Binds
     @IntoSet
