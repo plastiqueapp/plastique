96dbed6133beb68c488af991ed8b4580708a157b
technoir <sergey.chelombitko@gmail.com>
2019-09-08 23:35:01 +0100

    Disable animations in instrumented tests

--- tools/generate-screenshots.sh
+++ tools/generate-screenshots.sh
@@ -31,45 +31,8 @@ exit_demo_mode() {
     ${ADB} shell am broadcast -a com.android.systemui.demo -e command exit > /dev/null
 }
 
-disable_animations() {
-    echo "Disabling animations"
-
-    window_animation_scale=$(${ADB} shell settings get global window_animation_scale)
-    transition_animation_scale=$(${ADB} shell settings get global transition_animation_scale)
-    animator_duration_scale=$(${ADB} shell settings get global animator_duration_scale)
-    if [[ "$window_animation_scale" = "null" ]]; then
-        window_animation_scale=1.0
-    fi
-    if [[ "$transition_animation_scale" = "null" ]]; then
-        transition_animation_scale=1.0
-    fi
-    if [[ "$animator_duration_scale" = "null" ]]; then
-        animator_duration_scale=1.0
-    fi
-
-    ${ADB} shell settings put global window_animation_scale 0.0
-    ${ADB} shell settings put global transition_animation_scale 0.0
-    ${ADB} shell settings put global animator_duration_scale 0.0
-}
-
-restore_animations() {
-    echo "Restoring animations"
-
-    if [[ ! -z ${window_animation_scale+x} ]]; then
-        ${ADB} shell settings put global window_animation_scale ${window_animation_scale}
-    fi
-
-    if [[ ! -z ${transition_animation_scale+x} ]]; then
-        ${ADB} shell settings put global transition_animation_scale ${transition_animation_scale}
-    fi
-
-    if [[ ! -z ${animator_duration_scale+x} ]]; then
-        ${ADB} shell settings put global animator_duration_scale ${animator_duration_scale}
-    fi
-}
-
 run_tests() {
-    ./gradlew -q --console=plain :app:installDevDebug :app:installDevDebugAndroidTest
+    ./gradlew -q :app:installDevDebug :app:installDevDebugAndroidTest
     ${ADB} shell am instrument -w -e package io.plastique.test ${test_app_id}/io.plastique.test.PlastiqueJUnitRunner
 }
 
@@ -86,16 +49,10 @@ download_screenshots() {
     ${ADB} shell rm -r "$device_screenshot_path"
 }
 
-cleanup() {
-    exit_demo_mode
-    restore_animations
-}
-
 ensure_device
-trap cleanup EXIT
+trap exit_demo_mode EXIT
 
 enter_demo_mode
-disable_animations
 run_tests
 uninstall
 download_screenshots
