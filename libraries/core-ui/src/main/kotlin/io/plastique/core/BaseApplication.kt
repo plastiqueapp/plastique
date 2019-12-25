0ebf11be0b416445f4ad47f400ebea1d01ead392
technoir <sergey.chelombitko@gmail.com>
2019-12-25 21:49:10 +0000

    Implement missing split detection

--- libraries/core-ui/src/main/kotlin/io/plastique/core/BaseApplication.kt
+++ libraries/core-ui/src/main/kotlin/io/plastique/core/BaseApplication.kt
@@ -1,12 +1,18 @@
 package io.plastique.core
 
 import android.app.Application
+import com.google.android.play.core.missingsplits.MissingSplitsManagerFactory
 import io.plastique.inject.BaseActivityComponent
 import io.plastique.inject.BaseAppComponent
 
 abstract class BaseApplication : Application(), BaseAppComponent.Holder, BaseActivityComponent.Factory {
     override fun onCreate() {
         super.onCreate()
+
+        if (MissingSplitsManagerFactory.create(this).disableAppIfMissingRequiredSplits()) {
+            // Skip app initialization
+            return
+        }
 
         appComponent.initializers().asSequence()
             .sortedByDescending { it.priority }
