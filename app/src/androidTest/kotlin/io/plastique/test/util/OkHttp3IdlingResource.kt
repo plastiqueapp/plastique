4c960eee5233977face35851dcdbf68831f5b7a6
technoir <sergey.chelombitko@gmail.com>
2019-12-26 17:06:21 +0000

    Fix androidTest build

--- app/src/androidTest/kotlin/io/plastique/test/util/OkHttp3IdlingResource.kt
+++ app/src/androidTest/kotlin/io/plastique/test/util/OkHttp3IdlingResource.kt
@@ -1,15 +1,17 @@
 package io.plastique.test.util
 
 import androidx.test.espresso.IdlingResource
+import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 
-class OkHttp3IdlingResource(client: OkHttpClient, private val name: String) : IdlingResource {
-    private val dispatcher = client.dispatcher()
+class OkHttp3IdlingResource(private val dispatcher: Dispatcher, private val name: String) : IdlingResource {
     @Volatile private var callback: IdlingResource.ResourceCallback? = null
     @Volatile private var idle: Boolean = true
 
+    constructor(okHttpClient: OkHttpClient, name: String) : this(okHttpClient.dispatcher, name)
+
     init {
-        dispatcher.setIdleCallback {
+        dispatcher.idleCallback = Runnable {
             idle = true
             callback?.onTransitionToIdle()
         }
