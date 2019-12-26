03c2debdcd5c1b012529eec48865813f7ef29418
technoir <sergey.chelombitko@gmail.com>
2019-12-26 21:45:48 +0000

    Assert that schedulers are properly wrapped by RxIdler

--- app/src/androidTest/kotlin/io/plastique/test/RxIdlerInitializer.kt
+++ app/src/androidTest/kotlin/io/plastique/test/RxIdlerInitializer.kt
@@ -1,8 +1,10 @@
 package io.plastique.test
 
+import com.squareup.rx2.idler.IdlingResourceScheduler
 import com.squareup.rx2.idler.Rx2Idler
 import io.plastique.core.init.Initializer
 import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class RxIdlerInitializer @Inject constructor() : Initializer() {
@@ -11,6 +13,11 @@ class RxIdlerInitializer @Inject constructor() : Initializer() {
         RxJavaPlugins.setInitIoSchedulerHandler(Rx2Idler.create("Idling I/O Scheduler"))
         RxJavaPlugins.setInitSingleSchedulerHandler(Rx2Idler.create("Idling Single Scheduler"))
         RxJavaPlugins.setInitNewThreadSchedulerHandler(Rx2Idler.create("Idling New Thread Scheduler"))
+
+        check(Schedulers.computation() is IdlingResourceScheduler) { "Computation scheduler is already initialized" }
+        check(Schedulers.io() is IdlingResourceScheduler) { "IO scheduler is already initialized" }
+        check(Schedulers.single() is IdlingResourceScheduler) { "Single scheduler is already initialized" }
+        check(Schedulers.newThread() is IdlingResourceScheduler) { "New thread scheduler is already initialized" }
     }
 
     @Suppress("MagicNumber")
